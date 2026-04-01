ea636075b0f4c91bf97c553f483dfb9f480347ac
Edgwin <edgwin@gmail.com>
2026-04-01 12:05:15 -0600

    Se agrego el icono de editar en divs editables

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import {
   HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { NotificationService } from '../services/notification.service';
@@ -16,13 +16,15 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((err: HttpErrorResponse) => {
         if (err.status === 401 || err.status === 403) {
-          this.notificationService.show('info','La session termino, favor de ingresar sus credenciales nuevamente por favor');
+          this.notificationService.show('info','Tu sesión ha expirado, por favor inicia sesión nuevamente.');
           localStorage.removeItem('access_token');
           localStorage.removeItem('loggedUser');
           this.router.navigate(['/login']);
+          return EMPTY;
         }
         return throwError(() => err);
       })
     );
   }
 }
+
